6a12590c9e363db6afea284db92c77cbfb52a527
EddyTrotin <dev.eddy.trotin@outlook.com>
2016-12-14 01:19:25 +0100

    Added db handling class with static query method

--- connexionbdd.php
+++ connexionbdd.php
@@ -1,10 +1,21 @@
-<?php  //conexion base de donnée + verif error
-	try
-	{
-	$bdd = new PDO('mysql:host=localhost;dbname=MkT;charset=utf8', 'root', '');
-	}
-	catch (Exception $e)
-	{
-    die('Erreur : ' . $e->getMessage());
-	}
+<?php
+include_once('config/db.php');
+//db handling class, use static method query :
+//db::query('select * from table where data=?;', $value);
+class db{
+    public static function query($query){
+        try{
+            $pdo = new PDO('mysql:host='.Config::$host.';dbname='.Config::$dbname.';charset=utf8', Config::$user, Config::$mdp);
+        } catch (PDOException $e) {
+            error_log("Erreur !: " . $e->getMessage() . "<br/>");
+            die();
+        }
+        $args = func_get_args();
+        array_shift($args);
+        $reponse = $pdo->prepare($query);
+        $reponse->execute($args);
+        $pdo = null;
+        return $reponse;
+    }
+}
 ?>
